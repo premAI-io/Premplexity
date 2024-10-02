c1690d791c3ba223cad396e04e481aded6c8baab
D-Vitale <davidee.vitalee@gmail.com>
2024-10-02 18:05:56 +0200

    remove old font link

--- src/templates/layouts/partials/Head.tsx
+++ src/templates/layouts/partials/Head.tsx
@@ -33,18 +33,6 @@ const Head = ({
 
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link
-        rel="preconnect"
-        href="https://fonts.gstatic.com"
-        crossorigin="True"
-      />
-      <link
-        rel="preload"
-        href="https://fonts.gstatic.com/s/inter/v13/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa1ZL7W0Q5nw.woff2"
-        as="font"
-        type="font/woff2"
-        crossorigin="True"
-      />
-      <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Montserrat:wght@300;400;500;600;700&display=swap"
         rel="stylesheet"
       ></link>
